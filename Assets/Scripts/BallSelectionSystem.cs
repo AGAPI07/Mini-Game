2f8450d47a722e395141a44002cfd19bba2db277
AGAPI07 <104341224+AGAPI07@users.noreply.github.com>
2023-11-28 15:35:21 +0800

    just overthinking

--- Assets/Scripts/BallSelectionSystem.cs
+++ Assets/Scripts/BallSelectionSystem.cs
@@ -10,74 +10,91 @@ public class BallSelectionSystem : MonoBehaviour
     public GameObject player;
     SpriteRenderer playerSprite;
     int selNum;
-     private string selectedSkinIndex = "ssi";
+    private string selectedSkinIndex = "ssi";
     public GameEvents ge;
     bool fpl;
     public SkinListBinarySerDes slsb;
     private List<string> extractedNameList = new List<string>();
-    void SkinSelection(){
+    void SkinSelection()
+    {
         extractedNameList = slsb.DesNameList() as List<string>;
-        if(extractedNameList.Count <= 0 || extractedNameList == null){
+        if (extractedNameList.Count <= 0 || extractedNameList == null)
+        {
             // Pass
-        } else {
-        string selectedSkin = extractedNameList[selNum];
-        SpriteData spriteData = skinDatabase.SelectSkinAt(selectedSkin);
-        Debug.Log("Skin Name Selected: " + spriteData.skin_Name);
-        
-        playerSprite.sprite = spriteData._sprite;
-        if (menuBall == null){
-            //pass
-        }else {
-        menuBall.sprite = spriteData._sprite;}
         }
-        
-        
-    }
-    void TestFirstPlay(object ob, EventArgs e){
+        else
+        {
+            string selectedSkin = extractedNameList[selNum];
+            SpriteData spriteData = skinDatabase.SelectSkinAt(selectedSkin);
+            Debug.Log("Skin Name Selected: " + spriteData.skin_Name);
 
-            List <string> def = new  List<string>();
-            def.Add("White");
-            slsb.SerNameList(def);
-            ge.FirstPlayDone -= TestFirstPlay;
-            //PlayerPrefs.SetString("fpldn", "true");
-        
+            playerSprite.sprite = spriteData._sprite;
+            if (menuBall == null)
+            {
+                //pass
+            }
+            else
+            {
+                menuBall.sprite = spriteData._sprite;
+            }
+        }
+
+
+    }
+    void TestFirstPlay(object ob, EventArgs e)
+    {
+
+        List<string> def = new List<string>();
+        def.Add("White");
+        slsb.SerNameList(def);
+        ge.FirstPlayDone -= TestFirstPlay;
+        //PlayerPrefs.SetString("fpldn", "true");
+
         extractedNameList = slsb.DesNameList();
-        if(extractedNameList.Count <= 0 || extractedNameList == null){
+        if (extractedNameList.Count <= 0 || extractedNameList == null)
+        {
             // Pass
         }
         Debug.Log("Event Executed Recently");
     }
-    void Awake(){
+    void Awake()
+    {
         ge.FirstPlayDone += TestFirstPlay;
-        
+
     }
-    void Start(){
+    void Start()
+    {
         selNum = PlayerPrefs.GetInt(selectedSkinIndex);
         playerSprite = player.GetComponent<SpriteRenderer>();
         SkinSelection();
-    
-    }
-    
-    public void NextButton(){
-        selNum+=1;
-        if(selNum >= extractedNameList.Count){
-        selNum = 0;
-        }
-            SkinSelection();
 
     }
-    public void PrevButton(){
-        selNum-=1;
-        if(selNum <= -1){
-        selNum = extractedNameList.Count - 1;
-        }
-            SkinSelection();
 
-        
+    public void NextButton()
+    {
+        selNum += 1;
+        if (selNum >= extractedNameList.Count)
+        {
+            selNum = 0;
+        }
+        SkinSelection();
+
     }
-    public void EquipButton(){
-        PlayerPrefs.SetInt("mna",1);
-        PlayerPrefs.SetInt(selectedSkinIndex,selNum);
+    public void PrevButton()
+    {
+        selNum -= 1;
+        if (selNum <= -1)
+        {
+            selNum = extractedNameList.Count - 1;
+        }
+        SkinSelection();
+
+
+    }
+    public void EquipButton()
+    {
+        PlayerPrefs.SetInt("mna", 1);
+        PlayerPrefs.SetInt(selectedSkinIndex, selNum);
         SkinSelection();
     }
 }
--- Assets/Scripts/CoinSerDes.cs
+++ Assets/Scripts/CoinSerDes.cs
@@ -7,44 +7,48 @@ public class CoinSerDes : MonoBehaviour
 {
     public static CoinSerDes coinSerDes;
     /* COINDATA FORMATTER */
-    void Awake(){
+    void Awake()
+    {
         coinSerDes = this;
     }
     public void SaveCoin(CoinData x)
     {
         string coinStoragePath = Application.persistentDataPath + "/xxxx.rvc";
         Debug.Log(coinStoragePath);
-       FileStream  fileStream = new FileStream(coinStoragePath,FileMode.Create,FileAccess.Write);
-       BinaryFormatter bf = new BinaryFormatter();
+        FileStream fileStream = new FileStream(coinStoragePath, FileMode.Create, FileAccess.Write);
+        BinaryFormatter bf = new BinaryFormatter();
         Debug.Log("CoinSave Line Executed");
-        bf.Serialize(fileStream,x);
+        bf.Serialize(fileStream, x);
         fileStream.Close();
-       
+
     }
 
     public CoinData LoadCoin()
     {
-        
+
         string coinStoragePath = Application.persistentDataPath + "/xxxx.rvc";
         Debug.Log(coinStoragePath);
-       FileStream  fileStream = File.Open(coinStoragePath,FileMode.OpenOrCreate,FileAccess.Read);
+        FileStream fileStream = File.Open(coinStoragePath, FileMode.OpenOrCreate, FileAccess.Read);
         BinaryFormatter bf = new BinaryFormatter();
         Debug.Log("LoadFile Executed Successfully");
-        if (File.Exists(coinStoragePath) && fileStream.Length > 0) {
+        if (File.Exists(coinStoragePath) && fileStream.Length > 0)
+        {
             CoinData coinData = bf.Deserialize(fileStream) as CoinData;
             fileStream.Close();
             return coinData;
-        } else {
+        }
+        else
+        {
             fileStream.Close();
 
             Debug.Log("No coin file was found");
             return null;
         }
-        
-        
-        
+
+
+
     }
 
 
-    
+
 }
